977872139bd64d93f2f44660e04362063715eca5
Marcosvenasc <marcosvenasc@gmail.com>
2025-02-07 16:23:00 -0300

    finalizado

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tvglobo:tvglobo123@db:5432/youtube_crud'
@@ -8,9 +9,17 @@ db = SQLAlchemy(app)
 class URL(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return '<URL %r>' % self.url
+
+class APIKey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<APIKey %r>' % self.key
 
 @app.route('/urls', methods=['GET'])
 def get_urls():
@@ -20,13 +29,16 @@ def get_urls():
         url_data = {}
         url_data['id'] = url.id
         url_data['url'] = url.url
+        url_data['title'] = url.title
         output.append(url_data)
     return jsonify({'urls': output})
 
 @app.route('/urls', methods=['POST'])
 def add_url():
     url = request.json['url']
-    new_url = URL(url=url)
+    video_id = url.split('v=')[-1]
+    title = fetch_video_title(video_id)
+    new_url = URL(url=url, title=title)
     db.session.add(new_url)
     db.session.commit()
     return jsonify({'message': 'URL added!'})
@@ -40,7 +52,38 @@ def delete_url(id):
         return jsonify({'message': 'URL deleted!'})
     return jsonify({'message': 'URL not found!'})
 
+@app.route('/apikey', methods=['POST'])
+def add_apikey():
+    key = request.json['key']
+    existing_key = APIKey.query.first()
+    if existing_key:
+        existing_key.key = key
+    else:
+        new_key = APIKey(key=key)
+        db.session.add(new_key)
+    db.session.commit()
+    return jsonify({'message': 'API key added or updated!'})
+
+@app.route('/apikey', methods=['GET'])
+def get_apikey():
+    key = APIKey.query.first()
+    if key:
+        return jsonify({'key': key.key})
+    return jsonify({'key': None})
+
+def fetch_video_title(video_id):
+    key = APIKey.query.first().key 
+    response = requests.get(f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={key}&part=snippet')
+    return response.json()['items'][0]['snippet']['title']
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        
+        existing_key = APIKey.query.first()
+        if not existing_key:
+            new_key = APIKey(key='AIzaSyBPAdLXU5as0RhbAmKxhzOIsfpAHvCcSDE')
+            db.session.add(new_key)
+            db.session.commit()
+
     app.run(debug=True, host='0.0.0.0')
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
+import placeholderImage from './assets/globoplay.jpeg';
 
 function App() {
   const [urls, setUrls] = useState([]);
   const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
   const [playingUrl, setPlayingUrl] = useState('');
+  const [apiKey, setApiKey] = useState('');
 
   useEffect(() => {
     fetchUrls();
+    fetchApiKey();
   }, []);
 
   const fetchUrls = async () => {
@@ -15,56 +20,89 @@ function App() {
     setUrls(response.data.urls);
   };
 
+  const fetchApiKey = async () => {
+    const response = await axios.get('/apikey');
+    setApiKey(response.data.key);
+  };
+
+  const fetchVideoTitle = async (videoId) => {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`);
+    return response.data.items[0].snippet.title;
+  };
+
   const addUrl = async () => {
-    await axios.post('/urls', { url });
-    setUrl('');
-    fetchUrls();
+    const videoId = new URL(url).searchParams.get('v');
+    if (videoId) {
+      const videoTitle = await fetchVideoTitle(videoId);
+      await axios.post('/urls', { url, title: videoTitle });
+      setUrl('');
+      setTitle('');
+      fetchUrls();
+    } else {
+      alert('Por favor, forneça uma URL válida do YouTube.');
+    }
   };
 
   const deleteUrl = async (id) => {
-    await axios.delete(`/urls/${id}`);
-    fetchUrls();
+    if (window.confirm('Você realmente deseja deletar esta URL?')) {
+      await axios.delete(`/urls/${id}`);
+      fetchUrls();
+    }
+  };
+
+  const playVideo = (url) => {
+    setPlayingUrl(url);
   };
 
   return (
-    <div>
-      <h1>Desafio - YouTube URL Manager</h1>
-      <div style={{ marginBottom: '20px' }}>
+    <div className="container">
+      <h1 className="main-title">Desafio - Globo</h1>
+      <div className="input-container">
         <input
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
           placeholder="URL do YouTube"
-          style={{ marginRight: '10px' }}
         />
-        <button onClick={addUrl}>Adicionar</button>
+        <button className="add-button" onClick={addUrl}>Adicionar</button>
       </div>
-      <div style={{ marginBottom: '20px' }}>
-        <h2>Lista de URLs</h2>
-        <ul>
-          {urls.map((url) => (
-            <li key={url.id}>
-              {url.url}
-              <button onClick={() => deleteUrl(url.id)} style={{ marginLeft: '10px' }}>Deletar</button>
-              <button onClick={() => setPlayingUrl(url.url)} style={{ marginLeft: '10px' }}>Reproduzir Vídeo</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {playingUrl && (
-        <div>
-          <h2>Reproduzindo</h2>
+      <div className="iframe-container">
+        {playingUrl ? (
           <iframe
             width="560"
             height="315"
-            src={`https://www.youtube.com/embed/${new URL(playingUrl).searchParams.get("v")}`}
+            src={`https://www.youtube.com/embed/${new URL(playingUrl).searchParams.get("v")}?autoplay=1`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-        </div>
-      )}
+        ) : (
+          <img src={placeholderImage} alt="Quadro vazio" className="placeholder-image" />
+        )}
+      </div>
+      <div className="url-list">
+        <h2>Lista de URLs</h2>
+        <ul>
+          {urls.length > 0 ? (
+            urls.map((url) => (
+              <li key={url.id}>
+                <div className="icon-container">
+                  <div className="icon icon-fill" onClick={() => playVideo(url.url)} title="Reproduzir">
+                    <i className="fas fa-play"></i>
+                  </div>
+                  <div className="icon icon-delete" onClick={() => deleteUrl(url.id)} title="Excluir">
+                    <i className="fas fa-trash"></i>
+                  </div>
+                </div>
+                {url.title}
+              </li>
+            ))
+          ) : (
+            <p>Nenhuma URL adicionada ainda.</p>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
